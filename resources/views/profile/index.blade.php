3b8b764f20cc70fee6976d12f9335ae441580c43
Zerina <Zerina>
2017-12-19 18:16:52 +0100

    latest changes

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -8,11 +8,6 @@
                     <div class="panel-heading">My profile</div>
                     <div class="panel-body">
                         <h1>Hello, {{ Auth::user()->name }}</h1>
-                        @foreach($users as $user)
-                            <div>
-                                {{$user->name}}
-                            </div>
-                        @endforeach
                     </div>
                 </div>
             </div>
